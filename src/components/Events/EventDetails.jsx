7a93faf1ca2a9944f989973997c6d4b9a954c774
vvduth <ducthai060501@gmail.com>
2025-11-20 15:36:22 +0200

    feat: implement event details fetching and deletion functionality

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,83 @@
-import { Link, Outlet } from 'react-router-dom';
-
-import Header from '../Header.jsx';
-
+/* eslint-disable no-unused-vars */
+import { Link, Outlet, useParams } from "react-router-dom";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import Header from "../Header.jsx";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../utils/http.js";
+import { useNavigate } from "react-router-dom";
 export default function EventDetails() {
+  // id will be available from route params
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { data, isLoading, isPending, isError, error } = useQuery({
+    queryKey: ["events", id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+
+  // for deleting event
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    // need to pass ann object with id property to match deleteEvent function signature
+    mutate({ id: id });
+  }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Loading event...</p>
+      </div>
+    );
+  }
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title={"Failed to load event"}
+          message={error.info?.message || "Something went wrong"}
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <Outlet />
@@ -11,25 +86,7 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+      <article id="event-details">{content}</article>
     </>
   );
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -61,5 +61,38 @@ export async function fetchSelectableImages({ signal }) {
   return images;
 }
 
+
+// function to fetch a single event by id
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+// function to delete an event by id
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
+
 // general config for react query
 export const queryClient = new QueryClient();
