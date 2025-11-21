953b3984295cea6721d63fbfbedcc4ac03b6d416
vvduth <ducthai060501@gmail.com>
2025-11-21 09:33:09 +0200

    feat: implement event fetching to prepopulate edit form

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,10 +1,25 @@
-import { Link, useNavigate } from 'react-router-dom';
+// this component is used to edit an existing event.
+// for mow, it uses null as inputData for EventForm,
+// but in the future, it should fetch the existing event data
+// and pass it as inputData to EventForm.
+
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../utils/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';  
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+
+  // get the event to we get prepopulate the form
+  const { data , isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) =>  fetchEvent({signal, id: params.id}),
+  })
 
   function handleSubmit(formData) {}
 
@@ -12,9 +27,31 @@ export default function EditEvent() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+  if (isPending) {
+    content = <p className='center'>
+  <LoadingIndicator />
+    </p>;
+  }
+
+  if (isError) {
+    content = (
+      <>
+      <ErrorBlock 
+      title = "An error occurred"
+      message={error.info?.message|| "Failed to load event."} />
+      <div className='form-actions'>
+        <Link to="../" className="button">
+          Close
+        </Link>
+      </div>
+      </>
+    )
+  }
+
+  if (data ) {
+    content = (
+       <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +59,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
+    )
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+     {content}
     </Modal>
   );
 }
